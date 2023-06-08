7492e2a37fef0823fb3325d5acfc165822b3b626
cjh9622 <cjh9622@gmail.com>
2023-06-08 09:28:01 +0900

    댓글만들기

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-#테스트용
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+comments = []  # 댓글을 저장할 리스트
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/comments", methods=["GET", "POST"])
+def handle_comments():
+    if request.method == "GET":
+        return jsonify({"result": "success", "comments": comments})
+    elif request.method == "POST":
+        comment_text = request.form.get("comment")
+        if comment_text:
+            new_comment = {"author": "Anonymous", "text": comment_text}
+            comments.append(new_comment)
+            return jsonify({"result": "success"})
+        else:
+            return jsonify({"result": "error", "message": "Invalid comment."})
+
+if __name__ == "__main__":
+    app.run()
