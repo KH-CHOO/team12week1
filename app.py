0b4dfcb82c568b7287de22224caaaf894f9afd95
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-06-09 13:34:53 +0900

    Update app.py
    배포

--- app.py
+++ app.py
@@ -1,44 +1,64 @@
 from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
 from pymongo import MongoClient
+
+app = Flask(__name__)
+
 client = MongoClient('mongodb+srv://sparta:test@cluster0.iw4krrz.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
-import requests
-from bs4 import BeautifulSoup
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/movie", methods=["POST"])
-def movie_post():
-    url_receive = request.form['url_give']
-    star_receive = request.form['star_give']
-    comment_receive = request.form['comment_give']
-    
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive,headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
+@app.route("/members", methods=["GET"])
+def member_get():
+    all_members = list(db.members.find({},{'_id':False}))
+    return jsonify({'result':all_members})
 
-    ogtitle = soup.select_one('meta[property="og:title"]')['content']
-    ogimage = soup.select_one('meta[property="og:image"]')['content']
-    ogdesc = soup.select_one('meta[property="og:description"]')['content']
-    
+@app.route("/comments", methods=["POST"])
+def comment_post():
+    comments_list = list(db.comments.find({}, {'_id': False}))
+    count = len(comments_list) + 1
+
+    commentName_receive = request.form['comment_name_give']
+    commentPw_receive = request.form['comment_pw_give']
+    comment_receive = request.form['comment']
+
     doc = {
-        'title':ogtitle,
-        'desc':ogdesc,
-        'image':ogimage,
+        'comment-id':count,
+        'comment-name':commentName_receive,
+        'comment-pw':commentPw_receive,
         'comment':comment_receive,
-        'star':star_receive
     }
-    db.movies.insert_one(doc)
+    db.comments.insert_one(doc)
     
-    return jsonify({'msg':'DB에 저장함!'})
+    return jsonify({'msg':'작성완료!'})
 
-@app.route("/movie", methods=["GET"])
-def movie_get():
-    all_movies = list(db.movies.find({},{'_id':False}))
-    return jsonify({'result':all_movies})
+@app.route("/comments", methods=["GET"])
+def comment_get():
+    all_comments = list(db.comments.find({},{'_id':False}))
+    return jsonify({'result':all_comments})
+
+@app.route("/comments/<int:comment_id>", methods=["PUT"])
+def comment_put(comment_id):
+    comment = db.comments.find_one({'comment-id': comment_id})
+    if comment and comment['comment-pw'] == request.form.get('comment_pw_give'):
+        comment_receive = request.form.get('comment')
+        db.comments.update_one({'comment-id': comment_id}, {'$set': {'comment': comment_receive}})
+        return jsonify({'msg': '수정 완료!'}), 200
+    else:
+        return jsonify({'error': '잘못된 비밀번호입니다.'}), 404
+
+
+@app.route("/comments/<int:comment_id>", methods=["DELETE"])
+def comment_delete(comment_id):
+    comment = db.comments.find_one({'comment-id': comment_id})
+    if comment and comment['comment-pw'] == request.form.get('comment_pw_give'):
+        db.comments.delete_one({'comment-id': comment_id})
+        return jsonify({'msg': '삭제 완료!'}), 200
+    else:
+        return jsonify({'error': '잘못된 비밀번호입니다.'}), 404
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
